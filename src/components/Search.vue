47b39d2ad96df334d6d574c74f3e2d7440d280ca
seohwa0325 <seohwa0325@gmail.com>
2023-09-12 12:19:01 +0900

    search page

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -4,13 +4,14 @@
       v-model="title"
       class="form-control"
       type="text"
-      placeholder="search for movives, Series & more" />
+      placeholder="search for movives, Series & more" 
+      @keyup.enter="apply" />
   </div>
   <div class="selects">
     <select
       v-for="filter in filters"
-      :key="filters.name"
       v-model="$data[filter.name]"
+      :key="filter.name"
       class="form-select">
       <option
         v-if="filter.name === 'year'"
@@ -23,10 +24,17 @@
         {{ item }}
       </option>
     </select>
+
+    <button
+      class="btn btn-primary"
+      @click="apply">
+      Apply
+    </button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
     data() {
         return{
@@ -58,5 +66,42 @@ export default {
       ]
         }
     },
+    methods: {
+    async apply() {
+    const OMDB_API_KEY = '7035c60c'
+    const res = await axios.get(`https//www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${this.title}&type=${this.type}&y=${this.year}&page=1`)
+    console.log(res)
+    }
+  }
 }
 </script>
+
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  > * {
+    margin-right: 10px;
+    font-size: 15px;
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+  .selects {
+    display: flex;
+    select {
+      width:120px;
+      margin-right: 10px;
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+  }
+  .btn {
+    width: 120px;
+    height: 50px;
+    flex-shrink: 0;
+    font-weight: 700;
+  }
+}
+</style>
