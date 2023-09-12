147652c810d724da7c059cc304b4f8f648f53fd3
seohwa0325 <seohwa0325@gmail.com>
2023-09-12 14:02:32 +0900

    distict remove

--- src/store/movie.js
+++ src/store/movie.js
@@ -2,6 +2,8 @@
 
 const _defaultMessage = 'Search for the movie title!'
 import axios from 'axios'
+import _uniqBy from 'lodash/uniqBy'
+
 
 export default {
     namespaced : true,
@@ -28,36 +30,39 @@ export default {
     },
     //비동기
     actions : {
-        async searchMovies(state, context , payload){
+        async searchMovies(context , payload){
         const {title, type , number, year} =payload
         const OMDB_API_KEY = '7035c60c'
         const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
         const {Search ,totalResults} =res.data
         context.commit('updateState', {
-            movies :Search,
+            movies: _uniqBy(Search, 'imdbID')
+
             // message :'helloworld',
             // loading :true
         })
         console.log(totalResults); 
         console.log(typeof totalResults)
 
-        const total = parseInt(totalResults,10)
-        const pageLength =Math.ceil(total/10)
-
-        if(pageLength >1) {
-            for(let page =2 ; page <=pageLength; page +=1){
-                for (let page = 2; page <= pageLength; page += 1) {
-                    if (page > (payload.number / 10)) break
-                }
-                const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
-                const {Search}=res.data
-                context.commit('updateState', {
-                    movies :[...state.movies, ...Search],
-                    // message :'helloworld',
-                    // loading :true
-                })
-            }
-        }
-        }
+        // const total = parseInt(totalResults,10)
+        // const pageLength =Math.ceil(total/10)
+        // console.log(total);
+        // console.log(pageLength)
+            //추가요청
+        // if(pageLength >1) {
+        //     for(let page =2 ; page <= pageLength; page +=1){
+        //       if (page > number / 10){ 
+        //         break
+        //       }
+        //       const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
+        //       const {Search}=res.data
+        //         context.commit('updateState', {
+        //             movies :[...state.movies, ...Search],
+        //             // message :'helloworld',
+        //             // loading :true
+        //         })
+        //     }
+        // }
     }
+}
 }
