7138417a7fca21f9903372ec321044052df31387
seohwa0325 <seohwa0325@gmail.com>
2023-09-14 16:18:00 +0900

    loader

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,3 +1,53 @@
 <template>
-  <div></div>
+  <div 
+    :style="{ 
+      width: `${size}rem`, 
+      height: `${size}rem`,
+      zIndex
+    }"
+    :class="{
+      absolute,
+      fixed
+    }"
+    class="spinner-border text-primary"></div>
 </template>
+  
+  <script>
+  export default {
+    props: {
+      size: {
+        type: Number,
+        default: 2
+      },
+      absolute: {
+        type: Boolean,
+        default: false
+      },
+      fixed: {
+        type: Boolean,
+        default: false
+      },
+      zIndex: {
+        type: Number,
+        default: 0
+      }
+    }
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  @import "../scss/main";
+  .spinner-border {
+    margin: auto;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    &.absolute {
+      position: absolute;
+    }
+    &.fixed {
+      position: fixed;
+    }
+  }
+  </style>
