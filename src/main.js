b9818fcd7850de4973662b9cb9704b2e29793ae2
seohwa0325 <seohwa0325@gmail.com>
2023-09-18 15:04:47 +0900

    loadImage

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import store from './store'  // Same as './store/index.js'
 import router from './router' // Same as './router/index.js'
 import loadImage from './plugins/loadImage'
+
 createApp(App)
   .use(store)
   .use(router)
